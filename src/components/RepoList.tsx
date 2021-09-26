5d00c333e5bc06660bab162f797f785eef6aa691
keroken <kenichi.shinagawa@gmail.com>
2021-09-26 21:14:51 +0900

    connect RepoList to redux dispatch

--- src/components/RepoList.tsx
+++ src/components/RepoList.tsx
@@ -1,13 +1,49 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { searchRepo, searchRepoSuccess, searchRepoError } from '../state/repoList';
+import axios from 'axios';
 
 const RepoList: React.FC = () => {
   const [term, setTerm] = useState('');
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
+  console.log(state);
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTerm(event.target.value);
   };
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    
+    searchRepos(term);
+  };
+
+  const searchRepos = (term: string) => {
+    return async () => {
+      dispatch({
+        type: searchRepo
+      });
+
+      try {
+        const { data } = await axios.get('https://registry.npmjs.org/-/v1/search', {
+          params: {
+            text: term
+          }
+        });
+      
+        const names = data.objects.map((result: any) => {
+          return result.package.name;
+        });
+
+        dispatch({
+          type: searchRepoSuccess,
+          payload: names
+        });
+      } catch (err: any) {
+        dispatch({
+          type: searchRepoError,
+          payload: err.message
+        });
+      }
+    };
   };
 
   return (
