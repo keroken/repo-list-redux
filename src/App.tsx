4bf5edf11f308ed0bd41237ba286b539068e2ea7
keroken <kenichi.shinagawa@gmail.com>
2021-09-26 18:45:14 +0900

    create RepoList with form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { ListFormat } from 'typescript';
+import RepoList from './components/RepoList';
 
 function App() {
   return (
-    <h3>Repo List</h3>
+    <>
+      <h3>Repo List</h3>
+      <RepoList />
+    </>
   );
 }
 
